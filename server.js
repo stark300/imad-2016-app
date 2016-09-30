05e2ed38e72f9244089dea5510e1176aba4ab26a
stark300 <akwazza@gmail.com>
2016-09-30 14:06:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('art-one',function(req,res){
+    res.send("this is my article one");
+});
+app.get('art-two',function(req,res){
+    res.send("this is my article two");
+});
+app.get('art-three',function(req,res){
+    res.send("this is my article three");
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
